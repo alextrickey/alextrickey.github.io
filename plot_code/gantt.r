2be0e737dcc12daa71fdd09e4c6b7978213fe95e
Alex Trickey <k.alex.trickey@gmail.com>
2016-12-16 17:31:19 -0800

    feat: Add R code and data to create skills diagram
    The R file (gantt.r) creates and gantt chart from the data in the
    csv file.

---
+++ plot_code/gantt.r
@@ -0,0 +1,26 @@
+
+
+
+
+library(ggplot2)
+
+#Import Data
+skill_dates <- read.csv("~/Documents/resumes/dates.csv",
+                        strip.white = TRUE, 
+                        colClasses = c('factor','factor','Date','Date'))
+lapply(skill_dates,class)
+
+#Plot Based On: http://stackoverflow.com/questions/9862519
+ggplot(skill_dates, aes(colour=skill_type,
+                        group=skill_type)) + 
+  geom_segment(aes(x=Start, xend=End, 
+                   y=Skill, yend=Skill), 
+               size=5) +
+  xlab("Dates Used") +
+  ylab("") +
+  ggtitle("Relevant Skills by Dates Used") +
+  theme(legend.position="none",
+        text = element_text(size=15)) + 
+  facet_grid(skill_type~.,
+             scales = 'free',
+             space='free')
