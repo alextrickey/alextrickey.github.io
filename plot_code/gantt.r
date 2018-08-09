a9640a37f059146e246043bc90052f1f63c7e7c1
Alex Trickey <k.alex.trickey@gmail.com>
2018-08-08 20:49:03 -0700

    Update skills

--- plot_code/gantt.r
+++ plot_code/gantt.r
@@ -1,11 +1,13 @@
 
 library(ggplot2)
+library(lubridate)
 
 #Import Data
 skill_dates <- read.csv("plot_code/skill_dates.csv",
                         strip.white = TRUE,
                         colClasses = c("factor", "factor", "Date", "Date"),
                         comment.char = "#")
+skill_dates[is.na(skill_dates)] <- today()
 lapply(skill_dates, class)
 
 #Plot Based On: http://stackoverflow.com/questions/9862519
@@ -18,11 +20,12 @@ ggplot(skill_dates, aes(colour = skill_type,
   ggtitle("Technical Skills: Recency and Duration of Use") +
   theme(legend.position = "none",
         axis.title.y = element_blank(),
-        axis.text = element_text(color = "#ECE2CC"),
-        text = element_text(size = 15, color = "#ECE2CC"),
-        strip.background = element_rect(fill = "#A7A7A7"), # hex: #A7A7A7
-        strip.text = element_text(color = "#1D1D1D"), # hex: #A7A7A7
-        plot.background = element_rect(fill = "#1D1D1D")) +
+  #      axis.text = element_text(color = "#ECE2CC"),
+  #      text = element_text(size = 15, color = "#ECE2CC"),
+  #      strip.background = element_rect(fill = "#A7A7A7"), # hex: #A7A7A7
+  #      strip.text = element_text(color = "#1D1D1D"), # hex: #A7A7A7
+  #      plot.background = element_rect(fill = "#1D1D1D")
+  ) +
   facet_grid(skill_type ~ .,
              scales = "free",
              space = "free")
